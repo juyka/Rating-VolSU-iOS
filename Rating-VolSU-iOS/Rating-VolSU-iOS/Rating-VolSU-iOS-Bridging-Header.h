cc8a52b17417d0fc71856946389f60cb42d2d0f0
Настя <juyka@yahoo.com>
2014-10-08 15:34:47 +0400

    Добавила pods ObjectiveRecord, ObjectiveSugar, AFNetworking

---
+++ Rating-VolSU-iOS/Rating-VolSU-iOS/Rating-VolSU-iOS-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  BringHeader.h
+//  Rating-VolSU-iOS
+//
+//  Created by Настя on 08.10.14.
+//  Copyright (c) 2014 VolSU. All rights reserved.
+//
+
+#ifndef Rating_VolSU_iOS_BringHeader_h
+#define Rating_VolSU_iOS_BringHeader_h
+
+#import "ObjectiveRecord.h"
+#import "ObjectiveSugar.h"
+#import "AFNetworking/AFNetworking.h"
+
+#endif
